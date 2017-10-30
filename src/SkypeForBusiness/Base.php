2d322c07f3bdee6924acdd919270222048c42d11
Luca Pomero <lpomero@vdu.it>
2017-10-30 15:46:24 +0100

    Base class code standard

--- src/SkypeForBusiness/Base.php
+++ src/SkypeForBusiness/Base.php
@@ -1,86 +1,109 @@
-<?php namespace SkypeForBusiness;
+<?php
 
-class Base 
-{
-	/*************************************************
-	//	Variables
-	*************************************************/
-	
-	/*
-	 *	Server configuration
-	*/
-	protected static $ucwa_autodiscover = "http://lyncdiscover.example.com";
-	protected static $ucwa_fqdn = "";
-	protected static $ucwa_baseserver = "";
-	protected static $ucwa_path_oauth = "";
-	protected static $ucwa_path_user = "";
-	protected static $ucwa_path_xframe = "";
-	protected static $ucwa_path_application = "";
-	protected static $ucwa_path_application_fq = "";
-	protected static $ucwa_path_conversation = "";
-	protected static $ucwa_path_events = "";
-	
-	/*
-	 *	Storage
-	*/
-	protected static $ucwa_accesstoken = "";
-	protected static $ucwa_operationid = "";
-	protected static $ucwa_user = "";
-	protected static $ucwa_pass = "";
-	
-	public function __construct(Array $config = []) 
-	{	
-		// override the static properties defining the class config 
-		foreach ($config as $key => $setting) {
-			if (isset(self::$$key)) {
-				self::$$key = $setting;
-			}
-		}
-	}
+namespace SkypeForBusiness;
 
+/**
+ * Base class for config and common methods.
+ */
+class Base {
+  /*
+   * Variables
+   */
 
+  /*
+   * Server configuration
+   */
+  protected static $ucwa_autodiscover = "http://lyncdiscover.example.com";
+  protected static $ucwa_fqdn = "";
+  protected static $ucwa_baseserver = "";
+  protected static $ucwa_path_oauth = "";
+  protected static $ucwa_path_user = "";
+  protected static $ucwa_path_xframe = "";
+  protected static $ucwa_path_application = "";
+  protected static $ucwa_path_application_fq = "";
+  protected static $ucwa_path_conversation = "";
+  protected static $ucwa_path_events = "";
+  protected static $ucwa_grant_type = "password";
+  protected static $ucwa_use_ms_origin = TRUE;
 
-	/*************************************************
-	//	Helper methods
-	*************************************************/
-	
-	/*
-	 *	(void) _error
-	 *	######################################
-	 *
-	 *	Logging feature
-	*/
-	protected static function _error($text, $debug) {	
-		$file = fopen('ucwa.log', 'a');
-		fwrite($file, date("d-m-Y H:i:s") . ' | ' . $text . ' | ' . var_export($debug, true) . "\r\n");
-		fclose($file);
-	}
-	
-	/*
-	 *	(string) _generateUUID
-	 *	######################################
-	 *
-	 *	Generates an unique ID
-	*/
-	protected static function _generateUUID() {
-		return  str_replace(".", "", uniqid(md5( time() ), true));	
-	}	
-	
-	/*
-	 *	(array) getUCWAData
-	 *	######################################
-	 *
-	 *	Returns important information for
-	 *	connecting UCWA_init with UCWA_use,
-	 *	if they aren't running on the same
-	 *	instance.
-	*/
-	public static function getUCWAData() {
-		return array(
-			"accesstoken" => self::$ucwa_accesstoken,
-			"baseserver" => self::$ucwa_baseserver,
-			"path_application" => self::$ucwa_path_application,
-			"path_xframe" => self::$ucwa_path_xframe,
-		);	
-	}
+  /*
+   *  Storage
+   */
+  protected static $ucwa_accesstoken = "";
+  protected static $ucwa_operationid = "";
+  protected static $ucwa_user = "";
+  protected static $ucwa_pass = "";
+
+  /*
+   *  Base cURL config.
+   */
+  protected static $curl_base_config = array(
+    CURLOPT_RETURNTRANSFER => TRUE,
+    CURLOPT_SSL_VERIFYPEER => FALSE,
+    CURLOPT_SSL_VERIFYHOST => FALSE,
+  );
+
+  public function __construct(Array $config = []) {
+    // Override the static properties defining the class config.
+    foreach ($config as $key => $setting) {
+      if (isset(self::$$key)) {
+        self::$$key = $setting;
+      }
+    }
+  }
+
+  /*
+   *  Helper methods
+   */
+
+  /**
+   * Function _error()
+   *
+   * Logging feature.
+   */
+  protected static function _error($text, $debug) {
+    $file = fopen('ucwa.log', 'a');
+    fwrite($file, date("d-m-Y H:i:s") . ' | ' . $text . ' | ' . var_export($debug, TRUE) . "\r\n");
+    fclose($file);
+  }
+
+  /**
+   * Function_generateUUID().
+   *
+   * Generates an unique ID.
+   *
+   * @return string
+   *   The generated UUID.
+   */
+  protected static function _generateUUID() {
+    return str_replace(".", "", uniqid(md5(time()), TRUE));
+  }
+
+  /*
+   *  (array) getUCWAData
+   *  ######################################
+   *
+   *  Returns important information for
+   *  connecting UCWA_init with UCWA_use,
+   *  if they aren't running on the same
+   *  instance.
+   */
+  public static function getUCWAData() {
+    return array(
+      "accesstoken" => self::$ucwa_accesstoken,
+      "baseserver" => self::$ucwa_baseserver,
+      "path_application" => self::$ucwa_path_application,
+      "path_xframe" => self::$ucwa_path_xframe,
+    );
+  }
+
+  protected static function getDefaultHeaders(array $additional_headers = array()) {
+    $headers = array(
+      "Accept: application/json"
+    );
+    if (self::$ucwa_use_ms_origin) {
+      $headers[] = "X-Ms-Origin: " . self::$ucwa_fqdn;
+    }
+    return $headers + $additional_headers;
+  }
 }
